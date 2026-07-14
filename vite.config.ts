65921eea48eb6422dda14f1f5794a39a3008fd52
ahmetysnsr <44652123+ahmetysnsr@users.noreply.github.com>
2026-07-14 05:44:52 +0300

    fix: set exact base path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './', // GitHub Pages alt dizinleri için göreceli yol
+  base: '/bv-cafe-frontend/', // GitHub Pages alt dizinleri için mutlak yol
   plugins: [react()],
   server: {
     port: 5173,
